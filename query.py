bd1dc87441ccde6bccf0816ce2a3c815ed686565
Turjo Muttaqi <nmuttaqi@emich.edu>
2024-03-19 01:06:58 +0000

    Add cast option for movie search
    added a feature in query.py menu to enable users to search for movies by cast.
    
    Fixes #4

--- query.py
+++ query.py
@@ -7,6 +7,7 @@ def main():
         print("\nMenu:")
         print("• Enter list for listing all the movie names.")
         print("• Enter search for searching movies.")
+        print("• Enter cast for searching cast.")
         print("• Enter q for exit.")
         
         choice = input("Enter your choice: ").lower()
@@ -15,6 +16,8 @@ def main():
             list_movies(movies)
         elif choice == "search":
             search_movies(movies)
+        elif choice == "cast":
+            search_cast(movies)
         elif choice == 'q':
             break
         else:
@@ -36,7 +39,23 @@ def search_movies(movies):
         for movie in searched_movies:
             print(movie)
     else:
-        print(".")
+        print("No result.")
+
+def search_cast(movies):
+    cast = input("Enter partial cast name to search: ").lower()
+    searched_movies = []
+
+    for movie in movies._movies:
+        cast_list = [actor.lower() for actor in movie['cast']]
+        if any(cast in actor for actor in cast_list):
+            searched_movies.append((movie['name'], [actor for actor in movie['cast'] if cast in actor.lower()]))
+
+    if searched_movies:
+        for movie, cast in searched_movies:
+            print(movie)
+            print(cast)
+    else:
+        print("No result.")
 
 if __name__ == "__main__":
     main()
